a43bad73f4d658c6594bc717b0213d4c880f4605
邓学长 <976104701@qq.com>
2019-05-05 08:07:07 +0800

    e

--- src/main/java/org/jeecg/JeecgApplication.java
+++ src/main/java/org/jeecg/JeecgApplication.java
@@ -2,8 +2,11 @@ package org.jeecg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -13,4 +16,18 @@ public class JeecgApplication {
     	System.setProperty("spring.devtools.restart.enabled", "false");
     	SpringApplication.run(JeecgApplication.class, args);
     }
+    /**
+     * 设置上传文件大小，配置文件属性设置无效
+     *
+     * @author 空空dream
+     * @date 2018年3月8日
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory config = new MultipartConfigFactory();
+        config.setMaxFileSize("9000MB");
+        config.setMaxRequestSize("9000MB");
+        return config.createMultipartConfig();
+    }
+
 }
